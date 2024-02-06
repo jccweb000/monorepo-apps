02a114f2428eedbadf838742e0837840fd83b9cf
季长春 <jichangchun@jichangchundeMacBook-Pro.local>
2024-02-06 18:42:20 +0800

    feat: twice

--- .eslintrc.js
+++ .eslintrc.js
@@ -31,6 +31,7 @@ module.exports = {
         "react"
     ],
     "rules": {
-        "@typescript-eslint/no-var-requires": 0
+        "@typescript-eslint/no-var-requires": 0,
+        "@typescript-eslint/no-explicit-any": 1
     }
 }

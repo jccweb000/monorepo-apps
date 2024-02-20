0e48e18f98d9ce0edbd02eaf54bf28ef4a1e1012
季长春 <jichangchun@jichangchundeMacBook-Pro.local>
2024-02-20 11:17:44 +0800

    djdjdj

--- packages/twice/src/App.tsx
+++ packages/twice/src/App.tsx
@@ -5,17 +5,18 @@ import zhCN from 'antd/locale/zh_CN';
 import { BrowserRouter as Router } from 'react-router-dom';
 import AppLayout from './layout';
 
-
 type Locale = ConfigProviderProps['locale'];
 
 export const App = () => {
   const [locale] = React.useState<Locale>(zhCN);
-  
+
   return (
     <ConfigProvider locale={locale}>
       <Router>
         <AppLayout />
       </Router>
     </ConfigProvider>
-  )
-}
+  );
+};
+
+console.log('111');
